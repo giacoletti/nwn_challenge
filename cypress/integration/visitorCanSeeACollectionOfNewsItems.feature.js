8f7beb020f75d6ed8cff60beb1266c6c0df8e205
DefyCab <love.bellinder@gmail.com>
2021-11-26 13:18:55 +0100

    add user stories to README:MD. Begin writing test in Cypress.

--- cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
+++ cypress/integration/visitorCanSeeACollectionOfNewsItems.feature.js
@@ -1,5 +1,15 @@
-describe('', () => {
+/* eslint-disable no-undef */
+describe('User can see a a news feed', () => {
+
+  beforeEach(() => {
+  })
   it('', () => {
     cy.visit('/')
   });
 });
+
+// What do we need to test? Brainstorm
+// 
+// 1. Page loads
+// 2. The search bar has functionality
+// 3. Test the menus in App.jsx
