ad41389248dee43235dc2456a0721be5d8dfad00
giacoletti <giacoletti92@gmail.com>
2021-11-27 22:38:54 +0100

    Maps news fields in NewsCard.jsx creates index.css and fixes spacing between News cards

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -6,11 +6,11 @@ const NewsCard = (props) => {
   return (
     <Card
       data-cy={`news-${props.news.id}`}
-      image='https://editorial.fxstreet.com/images/Markets/Currencies/Digital%20Currencies/Bitcoin/bitcoins-52602600_Large.jpg'
+      image={props.news.urlToImage}
       header={props.news.title}
-      meta={`By: ${'Lorenzo Stroe'} at ${'FXStreet'}`}
-      description='DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu'
-      extra={<Header as='h6'>`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
+      meta={`By: ${props.news.author} at ${props.news.source.name}`}
+      description={props.news.description}
+      extra={<Header as='h6'>Published at {props.news.publishedAt}</Header>}
     />
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import configureStore from './state/store/configureStore'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './state/store/configureStore';
 
-const store = configureStore()
-window.store = store
+const store = configureStore();
+window.store = store;
 
 ReactDOM.render(
   <Provider store={store}>
